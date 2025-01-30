9afc4b72d98af8d26e77fdad9498f11100cf7d94
Jaewon Kim <jaewon.j.kim@snu.ac.kr>
2025-01-30 17:23:30 +0900

    add run shell script for modified code

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,1 @@
-# Original
-python script_testing.py \
-        --use_cuda     1\
-        --cuda_devices '1,2'
+python script_testing.py --model AHDR
---
+++ run_train.sh
@@ -0,0 +1,2 @@
+rm -rf trained-model-AHDR
+CUDA_VISIBLE_DEVICES=4,5,6,7 python script_training.py --model AHDR
